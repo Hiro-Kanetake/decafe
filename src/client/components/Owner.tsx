fddcfb5d5862cb97c4d582c3faa6168ea546de43
Hiro-Kanetake <git@github.com>
2022-08-10 00:27:21 +0900

    feat: Add filter function to render only owner shops

--- src/client/components/Owner.tsx
+++ src/client/components/Owner.tsx
@@ -26,7 +26,47 @@ const Owner: React.FC = () => {
     })();
   }, [ownerId]);
 
-  let displayShops = allShops.map((shop) => {
+  // This is just for mock-up to show ALL shops no matter what owner ID
+  // let displayShops = allShops.map((shop) => {
+  //   return (
+  //     <div className="shopInfo">
+  //       <div className="shopInfoText">
+  //         <label className="shopName" htmlFor={shop.id + "accordion"}>
+  //           {shop.name}
+  //         </label>
+  //         <br></br>
+  //         Address: <span className="shopAddress">{shop.address}</span>
+  //         <div className="imageWrapper">
+  //           <img
+  //             src="https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+  //             alt="cute coffee"
+  //           />
+  //         </div>
+  //         <div className="mapmenuwrapper">
+  //           <input
+  //             type="checkbox"
+  //             name=""
+  //             id={shop.id + "accordion"}
+  //             className="check"
+  //           />
+  //           <MenuMapWrapper shopInfo={shop} />
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // });
+
+  // This is for when we manage to implement shop create feature [Lines 58~93]
+  useEffect(() => {
+    // (async () => {
+      const filteredShops = allShops.filter((shop) => {
+        if (shop.owner_id == ownerId) return true;
+      })
+      setOwnerShops(filteredShops);
+    // })
+  }, [allShops]);
+
+  let displayShops = ownerShops.map((shop) => {
     return (
       <div className="shopInfo">
         <div className="shopInfoText">
@@ -55,21 +95,38 @@ const Owner: React.FC = () => {
     );
   });
 
-  return (
-    <>
-      <OwnerPortalHeader />
-      <div className="shopListWrapper">
-        <h2>
-          My Shops <span className="stationName" />
-        </h2>
-        <p className="addButton_owPortal">
-          <Link to="/shops/new">Add Shop</Link>
-        </p>
-        <div className="shopInfoWrapper">{displayShops}</div>
-        <div className="cafeShopPicWrapper"></div>
-      </div>
-    </>
-  );
+  if (ownerShops) {
+    return (
+      <>
+        <OwnerPortalHeader />
+        <div className="shopListWrapper">
+          <h2>
+            My Shops <span className="stationName" />
+          </h2>
+          <p className="addButton_owPortal">
+            <Link to="/shops/new">Add Shop</Link>
+          </p>
+          <div className="shopInfoWrapper">{displayShops}</div>
+          <div className="cafeShopPicWrapper"></div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <OwnerPortalHeader />
+        <div className="shopListWrapper">
+          <h2>
+            My Shops <span className="stationName" />
+          </h2>
+          <p className="addButton_owPortal">
+            <Link to="/shops/new">Add Shop</Link>
+          </p>
+        </div>
+      </>
+    );
+  }
+  
 };
 
 export default Owner;
