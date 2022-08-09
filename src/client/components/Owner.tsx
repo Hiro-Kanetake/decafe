458af99f13ec46a1aa3f61ed240361cc857ab1d8
Hiro-Kanetake <git@github.com>
2022-08-09 21:18:46 +0900

    feat: Add header and shop display on owner page

--- src/client/components/Owner.tsx
+++ src/client/components/Owner.tsx
@@ -2,20 +2,33 @@ import React, { useEffect, useState } from "react";
 import "../styles/ShopInfo.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { findAllShops } from "../api";
-
-interface AllShopsInfo {
-  id: number;
-  name: string;
-  address: string;
-}
+import { decodeOwnerId } from "../api";
+import { ShopInfo } from "../../share/model.type";
+import MenuMapWrapper from "./MenuMapWrapper";
+import OwnerPortalHeader from "./OwnerPortalHeader";
 
 const Owner: React.FC = () => {
-  const [allShops, setAllShops] = useState<AllShopsInfo[]>([]);
+  const [allShops, setAllShops] = useState<ShopInfo[]>([]);
+  const [ownerId, setOwnerId] = useState<number>();
+  const [ownerShops, setOwnerShops] = useState<ShopInfo[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const id = await decodeOwnerId();
+      setOwnerId(id);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const result = await findAllShops();
+      setAllShops(result);
+    })();
+  }, [ownerId]);
 
   let displayShops = allShops.map((shop) => {
     return (
       <div className="shopInfo">
-        <Link to={"/shops/" + shop.id}>
           <div className="shopInfoText">
             <span className="shopName">{shop.name}</span>
             <br></br>
@@ -26,56 +39,23 @@ const Owner: React.FC = () => {
                 alt="cute coffee"
               />
             </div>
+            <div className="mapmenuwrapper">
+              <MenuMapWrapper shopInfo={shop} />
+            </div>
           </div>
-        </Link>
       </div>
     );
   });
 
-  useEffect(() => {
-    (async () => {
-      const result = await findAllShops();
-      console.log(result);
-    })();
-  }, [allShops, setAllShops]);
-
   return (
-    <>
-      <p>
-        <Link to="/owners/logout">Logout</Link>
-      </p>
-      <div className="owner">
-        <p>
-          <Link to="/shops/new">Create Shop</Link>
-        </p>
-        <div className="shopInfo_ow">
-          <div className="shopInfoPhoto_ow">
-            <img src="" alt="" />
-          </div>
-          <div className="shopInfoText_ow">
-            <h2>shop name</h2>
-            <p>〒166-0003 東京都杉並区高円寺南4丁目9-6 第三矢島ビル 2F</p>
-            <label className="open" htmlFor="pop-up">
-              Menu
-            </label>
-            <input type="checkbox" id="pop-up" />
-            <div className="overlay">
-              <div className="window">
-                <label className="close" htmlFor="pop-up">
-                  x
-                </label>
-                <ul>
-                  <li>Hot coffee</li>
-                  <li>Ice coffee</li>
-                  <li>Jam Toast</li>
-                  <li>Pizza Toast</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="shopListWrapper">
+      <OwnerPortalHeader />
+      <h2>
+        Shops in <span className="stationName"/>
+      </h2>
+      <div className="shopInfoWrapper">{displayShops}</div>
+      <div className="cafeShopPicWrapper"></div>
+    </div>
   );
 };
 
