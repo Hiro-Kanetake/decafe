050a7c9e3baff81d4218efbba70829566c43f120
keigrey <keixgrey@gmail.com>
2022-08-10 13:39:49 +0900

    fix: Fix Header after git conflict

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -5,9 +6,20 @@ import {
   useNavigate,
 } from "react-router-dom";
 import "../styles/header.css";
+import { decodeOwnerId } from "../api";
 
 const Header = () => {
   const navigate = useNavigate();
+  const [buttonText, setButtonText] = useState<string>("");
+  useEffect(() => {
+    decodeOwnerId()
+      .then(() => {
+        setButtonText("My Portal");
+      })
+      .catch(() => {
+        setButtonText("Owner Portal");
+      });
+  }, []);
   return (
     <header className="App-header">
       <div className="header header_ow">
@@ -24,7 +36,19 @@ const Header = () => {
         </h1>
         <div>
           <Link to="/" reloadDocument></Link>
-          <button onClick={() => navigate("./owners/new")}>Owner Portal</button>
+          <button
+            onClick={() => {
+              decodeOwnerId()
+                .then(() => {
+                  navigate("./owners");
+                })
+                .catch(() => {
+                  navigate("./owners/access");
+                });
+            }}
+          >
+            {buttonText}
+          </button>
         </div>
       </div>
     </header>
