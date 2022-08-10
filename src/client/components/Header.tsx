99e53515e97d94057348f683f23ff67f491c0d27
keigrey <keixgrey@gmail.com>
2022-08-10 12:34:11 +0900

    fix: Copy Header from github

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -5,13 +5,30 @@ import {
   useNavigate,
 } from "react-router-dom";
 import "../styles/header.css";
-import { decodeOwnerId } from "../api";
-import { useEffect, useState } from "react";
 
 const Header = () => {
   const navigate = useNavigate();
-
-  return <></>;
+  return (
+    <header className="App-header">
+      <div className="header header_ow">
+        <h1 className="ownerWrapper">
+          {/* <Link to="/">Home</Link> */}
+          <Link to="/" reloadDocument>
+            <img
+              src="../images/decafelogo.png"
+              className="logo"
+              alt="logo"
+            ></img>
+            {/* src="../../../public/images/decafelogo.png" */}
+          </Link>
+        </h1>
+        <div>
+          <Link to="/" reloadDocument></Link>
+          <button onClick={() => navigate("./owners/new")}>Owner Portal</button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
