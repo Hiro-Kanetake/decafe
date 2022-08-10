d47bbce84655766a9df180ee03b805de926b680b
keigrey <keixgrey@gmail.com>
2022-08-10 11:04:53 +0900

    feat: Be able to go back to owner portal without logging in again

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -5,6 +5,7 @@ import {
   useNavigate,
 } from "react-router-dom";
 import "../styles/header.css";
+import { decodeOwnerId } from "../api";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -12,7 +13,19 @@ const Header = () => {
     <header className="App-header">
       <div className="header">
         <Link to="/" reloadDocument></Link>
-        <button onClick={() => navigate("./owners/new")}>Owner Portal</button>
+        <button
+          onClick={() => {
+            decodeOwnerId()
+              .then(() => {
+                navigate("./owners/id");
+              })
+              .catch(() => {
+                navigate("./owners/new");
+              });
+          }}
+        >
+          Owner Portal
+        </button>
       </div>
     </header>
   );
